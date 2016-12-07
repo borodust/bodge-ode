d8f6b3950db70b2d3818068fe849c6ebb41d2c3a
Pavel Korolev <dev@borodust.org>
2016-12-07 22:24:57 +0300

    Infinity value for double float version of ODE

--- main.lisp
+++ main.lisp
@@ -25,16 +25,31 @@
 
 
 (define-constant +infinity+
-    #+sbcl sb-ext:single-float-positive-infinity
-    #+clozure 1S++0
-    #+abcl ext:single-float-positive-infinity
-    #+allegro excl::*infinity-single*
-    #+cmu ext:single-float-positive-infinity
-    #+(and ecl (not infinity-not-available)) si:single-float-positive-infinity
-    #+lispworks (coerce infinity$$ 'single-float)
-    #+scl ext:single-float-positive-infinity
-    #-(or sbcl clozure abcl allegro cmu ecl lispworks scl)
-    most-positive-single-float :test 'eql)
+    (if (search "double_precision" (%ode:get-configuration))
+        (progn
+          #+sbcl sb-ext:double-float-positive-infinity
+          #+clozure 1D++0
+          #+abcl ext:double-float-positive-infinity
+          #+allegro excl::*infinity-double*
+          #+cmu ext:double-float-positive-infinity
+          #+(and ecl (not infinity-not-available)) si:double-float-positive-infinity
+          #+lispworks #.(read-from-string "10E999")
+          #+scl ext:double-float-positive-infinity
+          #-(or sbcl clozure abcl allegro cmu ecl lispworks scl)
+          most-positive-double-float)
+        (progn
+          #+sbcl sb-ext:single-float-positive-infinity
+          #+clozure 1S++0
+          #+abcl ext:single-float-positive-infinity
+          #+allegro excl::*infinity-single*
+          #+cmu ext:single-float-positive-infinity
+          #+(and ecl (not infinity-not-available)) si:single-float-positive-infinity
+          #+lispworks (coerce infinity$$ 'single-float)
+          #+scl ext:single-float-positive-infinity
+          #-(or sbcl clozure abcl allegro cmu ecl lispworks scl)
+          most-positive-single-float))
+  :test 'eql)
+
 
 (defmacro define-collision-callback (name (collision-input this-geom that-geom) &body body)
   (with-gensyms (data-ptr g0 g1)
