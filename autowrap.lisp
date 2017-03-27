67f6cb8e78a7352544198fec66d8247c6626e530
Pavel Korolev <dev@borodust.org>
2017-03-27 21:58:58 +0300

    Add Windows build

--- autowrap.lisp
+++ autowrap.lisp
@@ -4,6 +4,10 @@
 (c-include '(bodge-ode spec "lib.h")
  :spec-path '(bodge-ode spec)
  :definition-package :%ode
+ :sysincludes (append #+windows
+		      (list "c:/msys64/mingw64/x86_64-w64-mingw32/include/"
+			    "c:/msys64/mingw64/include/"
+			    "c:/msys64/usr/local/include/"))
  :exclude-sources (".*.h")
  :include-sources ("ode/odeconfig.h"
                    "ode/common.h"
@@ -15,7 +19,12 @@
                    "ode/collision_space.h"
                    "ode/collision.h"
                    "ode/version.h")
- :exclude-definitions ("dWorldSetStepThreadingImplementation")
+ :exclude-definitions ("dWorldSetStepThreadingImplementation"
+		       "vswprintf")
+ :include-definitions (;; common
+		       "^(__)?(u|u_)?int.{0,2}(ptr)?(_t)?$"
+		       "wchar_t"
+		       "size_t")
  :symbol-regex (("dx?[A-Z]\\w*" () (lambda (name matches regex)
                                    (declare (ignore matches regex))
                                    (subseq name 1)))))
