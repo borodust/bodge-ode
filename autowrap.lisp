dd575b0beee5b4117b5b8e39e36b92e8b5bdbd4e
Pavel Korolev <dev@borodust.org>
2017-09-17 17:49:53 +0300

    Use :bodge-autowrap

--- autowrap.lisp
+++ autowrap.lisp
@@ -1,26 +1,16 @@
 (in-package :bodge-ode)
 
 
-(c-include '(bodge-ode "lib.h")
- :spec-path '(bodge-ode spec)
- :definition-package :%ode
- :sysincludes (append #+windows
-		      (list "c:/msys64/mingw64/x86_64-w64-mingw32/include/"
-			    "c:/msys64/mingw64/include/"
-			    "c:/msys64/usr/local/include/"))
- :exclude-sources (".*.h")
- :include-sources ("ode/odeconfig.h"
-                   "ode/common.h"
-                   "ode/odeinit.h"
-                   "ode/contact.h"
-                   "ode/error.h"
-                   "ode/mass.h"
-                   "ode/objects.h"
-                   "ode/collision_space.h"
-                   "ode/collision.h"
-                   "ode/version.h")
- :exclude-definitions ("dWorldSetStepThreadingImplementation")
- :filter-spec-p t
- :symbol-regex (("dx?[A-Z]\\w*" () (lambda (name matches regex)
-                                   (declare (ignore matches regex))
-                                   (subseq name 1)))))
+(bodge-autowrap:c-include "lib.h" bodge-ode
+  :package :%ode
+  :include-sources ("ode/odeconfig.h"
+                    "ode/common.h"
+                    "ode/odeinit.h"
+                    "ode/contact.h"
+                    "ode/error.h"
+                    "ode/mass.h"
+                    "ode/objects.h"
+                    "ode/collision_space.h"
+                    "ode/collision.h"
+                    "ode/version.h")
+  :symbol-regex (("dx?[A-Z]\\w*" 1)))
