b23b0337be407b84f908251324159ac5df0b8653
Pavel Korolev <dev@borodust.org>
2018-02-04 23:25:22 +0300

    Update interface and specs

--- claw.lisp
+++ claw.lisp
@@ -1,14 +1,5 @@
 (claw:c-include "bodge_ode.h" bodge-ode
   :in-package :%ode
   :sysincludes (:ode-includes)
-  :include-sources ("ode/odeconfig.h"
-                    "ode/common.h"
-                    "ode/odeinit.h"
-                    "ode/contact.h"
-                    "ode/error.h"
-                    "ode/mass.h"
-                    "ode/objects.h"
-                    "ode/collision_space.h"
-                    "ode/collision.h"
-                    "ode/version.h")
+  :include-definitions ("dx?[A-Z]\\w*")
   :rename-symbols (claw:by-removing-complex-prefix "dx?[A-Z]\\w*" 1))
